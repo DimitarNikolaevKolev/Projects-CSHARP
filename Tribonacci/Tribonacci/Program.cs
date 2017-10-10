61026a8574fe7c561f3ecf69dbc3bbb889efad12
DimitarNikolaevKolev <dnk_@abv.bg>
2017-10-10 20:07:11 +0300

    Add files via upload

---
+++ Tribonacci/Tribonacci/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Tribonacci
+{
+    class Program
+    {
+        static void Main()
+        {
+            BigInteger number1 = BigInteger.Parse(Console.ReadLine());
+            BigInteger number2 = BigInteger.Parse(Console.ReadLine());
+            BigInteger number3 = BigInteger.Parse(Console.ReadLine());
+
+            int numberN = int.Parse(Console.ReadLine());
+
+
+            if (numberN > 3)
+            {
+                for (int i = 3; i < numberN; i++)
+                {
+                    BigInteger tempNum = number1 + number2 + number3;
+                    number1 = number2;
+                    number2 = number3;
+                    number3 = tempNum;
+                }
+                Console.WriteLine(number3);
+            }
+
+            if (numberN == 1)
+            {
+                Console.WriteLine(number1);
+            }
+
+            if (numberN == 2)
+            {
+                Console.WriteLine(number2);
+            }
+
+            if (numberN == 3)
+            {
+                Console.WriteLine(number3);
+            }
+
+
+
+        }
+    }
+}
---
+++ UK-FLAG/UK-FLAG/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace UK_FLAG
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numberN = int.Parse(Console.ReadLine());
+            int uppRows = numberN / 2;
+            int dotsSideUp = 0;
+            int dotsIn = (numberN / 2) - 1;
+
+
+
+            for (int i = 0; i < uppRows; i++) // Горна част на Великогритаския флаг
+            {
+                Console.Write(new string('.', dotsSideUp));
+                Console.Write("\\");
+                Console.Write(new string('.', dotsIn));
+                Console.Write("|");
+                Console.Write(new string('.', dotsIn));
+                Console.Write("/");
+                Console.Write(new string('.', dotsSideUp));
+
+                dotsIn--;
+                dotsSideUp++;
+                Console.WriteLine();
+            }
+
+            
+
+            Console.Write(new string('-', uppRows));    // Среда на Великогритаския флаг
+            Console.Write("*");                          // Среда на Великогритаския флаг
+            Console.Write(new string('-', uppRows));       // Среда на Великогритаския флаг
+            Console.WriteLine();
+
+            dotsSideUp = (numberN / 2) - 1;
+            dotsIn = 0;
+            for (int i = 0; i < uppRows; i++) //Долна част на Великогритаския флаг
+            {
+                Console.Write(new string('.', dotsSideUp));
+                Console.Write("/");
+                Console.Write(new string('.', dotsIn));
+                Console.Write("|");
+                Console.Write(new string('.', dotsIn));
+                Console.Write("\\");
+                Console.Write(new string('.', dotsSideUp));
+
+                dotsSideUp--;
+                dotsIn++;
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine();
+        }
+    }
+}
